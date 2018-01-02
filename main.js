2bdb8173d827542d92aaf57307b5692d7834f34b
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2018-01-03 00:02:00 +0100

    Emails 😅

--- main.js
+++ main.js
@@ -14,34 +14,8 @@
           Main.pintaDatos( data );
         }
         else {
-          Main.peticionAjax( `https://api.github.com/users/${ Main.user }/events`, function ( data_events ) {
-            // https://regex101.com/r/gm32zb/1/codegen?language=javascript
-            let emails = [];
-            const regex = /(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
-            let m;
-            while ( (m = regex.exec( data_events )) !== null ) {
-              // This is necessary to avoid infinite loops with zero-width matches
-              if ( m.index === regex.lastIndex ) {
-                regex.lastIndex++;
-              }
-
-              // The result can be accessed through the `m`-variable.
-              m.forEach( function ( match, groupIndex ) {
-                const badEmails = [ "git@github.com", "badges@fossa.io" ];
-                if ( badEmails.indexOf( match ) === -1 ) {
-                  emails.push( match );
-                }
-              } );
-            }
-
-            // https://stackoverflow.com/a/14438954
-            function onlyUnique( value, index, self ) {
-              return self.indexOf( value ) === index;
-            }
-
-            data.email = emails.filter( onlyUnique );
-            Main.pintaDatos( data );
-          } );
+          data.email = '🌈🦄🎆';
+          Main.pintaDatos( data );
         }
       } );
     },
@@ -69,7 +43,7 @@
         <a href="http://github.com/baumannzone/awesomico-css">
           <img class="ribbon" src="assets/ribbon.png" alt="fork me on github">
         </a>
-        <img src="${data.avatar_url}">
+        <img class="profile" src="${data.avatar_url}">
         <p class="animation-9 name">Name: <span>${data.name} (${data.login})</span></p>
         <p class="animation-2 email">Email: <span>${data.email ? data.email : "Not found"}</span></p>
         <p class="animation-3 bio">Bio: <span>${data.bio}</span></p>
